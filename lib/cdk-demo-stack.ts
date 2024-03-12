cc4da850c17cbd7aee1f2a1072ced84591c05131
toshiaki-suzuki <saki0x4bf@gmail.com>
2024-03-12 09:42:17 +0900

    Add vpc and public subnets

--- lib/cdk-demo-stack.ts
+++ lib/cdk-demo-stack.ts
@@ -1,16 +1,22 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 
 export class CdkDemoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkDemoQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new ec2.Vpc(this, 'mainVPC', {
+      // This is where you can define how many AZs you want to use
+      maxAzs: 2,
+      // This is where you can define the subnet configuration per AZ
+      subnetConfiguration: [
+         {
+           cidrMask: 24,
+           name: 'public-subnet',
+           subnetType: ec2.SubnetType.PUBLIC,
+         }
+      ]
+   });
   }
 }
